1f0351fec475279c19855fda672b0770c7825aeb
Patrik Bóna <patrik.bona@mrhead.sk>
2015-03-12 11:37:01 +0100

    Refactor `ApplicationController`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authenticate
-  before_filter :set_contexts
+  before_filter :authenticate, :set_contexts
 
   attr_reader :current_user
   helper_method :current_user
 
   private
 
-  def set_contexts
-    @contexts = current_user.contexts if current_user
-  end
-
   def authenticate
     unless @current_user = User.find_by(id: session[:user_id])
       redirect_to signin_path
     end
+  end
+
+  def set_contexts
+    @contexts = current_user.contexts if current_user
   end
 
   def sign_in(user)
