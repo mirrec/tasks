6427ce61c2b4f6dd84bba6dd0d34a714ae356860
Patrik Bóna <patrik.bona@mrhead.sk>
2015-03-12 14:52:28 +0100

    Do not use `@context` in projects views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     return "#{context.name}" if context.projects_for_today.count == 0
     "#{context.name} (#{context.projects_for_today.count})"
   end
+
+  def shallow_args(parent, child)
+    child.try(:new_record?) ? [parent, child] : child
+  end
 end
